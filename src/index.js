e3cf453419ae235f14dc5eb290ba8fda9eca3db5
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-06 16:55:26 -0800

    dummy <List/> works

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Wrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewType: 'list',
+    };
+  }
+
+  //logic
+
+  render() {    
+    return (
+      <div><List/></div>
+    );
+  }
+}
+
+function List(props){
+  return (
+    <ul>
+      <li>stuff1</li>
+      <li>stuff2</li>
+    </ul>
+  );
+}
 
 
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <Wrapper />,
   document.getElementById('root')
 );
