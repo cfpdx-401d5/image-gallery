4cbe6f5f3f782b8b2844d4b0bbdd78f70735cf9f
mericks <mericks@users.noreply.github.com>
2017-03-07 12:27:32 -0800

    add ListView

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import images from './Images.js';
 
+const ListView = () => {
+    return(
+        <div>
+            <ul>
+                <li>{images[0].title}</li>
+                <li>{images[0].description}</li>
+                <li>{images[0].url}</li>
+            </ul>
+        </div>
+    )
+}
+
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            currentDisplay: ListView,
+        };
     };
-
-    viewList() {
-        return (
-            <div>
-                <ul>
-                    <li>{images[0].title}</li>
-                </ul>
-            </div>
-        )
-    }   
-
 
     render() {
         return (
             <div>
-                <h1>Hello, World. This is the separated App!</h1>
+                <ListView />
             </div>
         )
     }
