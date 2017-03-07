7ab306f178bd41d5b2f8186046bdc6285cf7239c
mericks <mericks@users.noreply.github.com>
2017-03-07 10:39:54 -0800

    create viewList component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import images from './Images.js';
 
 export default class App extends Component {
+
+    viewList() {
+        return (
+            <div>
+                <ul>
+                    <li>{images[0].title}</li>
+                </ul>
+            </div>
+        )
+    }   
+
+
     render() {
         return (
             <div>
