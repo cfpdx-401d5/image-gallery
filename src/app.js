ac1aa5a44729c6c7aeefdce0cf4705d6a2bb094f
sdcaulley <stephanie.caulley@gmail.com>
2017-03-07 15:54:07 -0800

    clean up

--- src/app.js
+++ src/app.js
@@ -5,51 +5,40 @@ import { Thumbnail } from './view/thumbnail-view';
 
 import bunnies from './image';
 
+const GALLERY_TYPE    = 'gallery';
+const LIST_TYPE       = 'list';
+const THUMBNAIL_TYPE  = 'thumbnail';
+
 function ViewSelector(props) {
     return (
       <div>
-        <button onClick={() => props.onClick('gallery')}>Gallery View</button>
-        <button onClick={() => props.onClick('list')}>List View</button>
-        <button onClick={() => props.onClick('thumbnail')}>Thumbnail View</button>
+        <button onClick={() => props.onClick(GALLERY_TYPE)}>Gallery View</button>
+        <button onClick={() => props.onClick(LIST_TYPE)}>List View</button>
+        <button onClick={() => props.onClick(THUMBNAIL_TYPE)}>Thumbnail View</button>
       </div>
     );
 }
 
 function ImageView(props) {
     const bunnyArray = bunnies();
-    switch (props.view) {
-        case 'list':
-            return (
-                <div>
-                   <ViewSelector onClick={props.onClick}/>
-                   <List array={bunnyArray} />;
-                </div>
-            );
 
-        case 'gallery':
-            return (
-                <div>
-                    <ViewSelector onClick={props.onClick}/>
-                    <Gallery array={bunnyArray} />;
-                </div>
-            );
+    const outputs = {
+        [LIST_TYPE]: List,
+        [GALLERY_TYPE]: Gallery,
+        [THUMBNAIL_TYPE]: Thumbnail,
+    };    
+    
+    const OutputComponent = outputs[props.view];
 
-        case 'thumbnail':
-            return (
-                <div>
-                    <ViewSelector onClick={props.onClick}/>
-                    <Thumbnail array={bunnyArray} />
-                </div>
-            );
-
-        default:
-            return (
-                <div>
-                    <ViewSelector onClick={props.onClick}/>
-                    Click a button to view bunnies!
-                </div>
-            );
-    }
+    return (
+        <div>
+            <ViewSelector onClick={props.onClick}/>
+            {OutputComponent 
+              ? <OutputComponent array={bunnyArray} />
+              : 'Click a button to view bunnies!'
+            }          
+        </div>
+    );
 }
 
 export default class App extends React.Component {
