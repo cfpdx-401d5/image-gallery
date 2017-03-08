372418bd4d33b0636528382454045dbbc29d3767
mizutombo <thomas.shultz52@gmail.com>
2017-03-08 11:47:09 -0800

    repairs code in images.js

--- src/images.js
+++ src/images.js
@@ -1,17 +1,19 @@
 
 const images = [
   {
+    id: new Date(),
     title: 'Cute Bunny',
     description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
     url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'   
   },
   {
+    id: new Date(),
     title: 'Snow Flake',
     description: 'White Rabbit',
     url: 'http://im5.leaderhero.com/wallpaper/20140328/12835c9a-f.jpg'
   },
 ];
 
-export default function bunnyList() {
+export default function ImagesList() {
   return images;
 }
