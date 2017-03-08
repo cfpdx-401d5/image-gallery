5f282f1cd3659695e672428a2fc5576861dd8608
Clarefollett <chlaw101@gmail.com>
2017-03-08 14:51:49 -0800

    first passing snapshot!

--- image-gallery/tests/App.test.js
+++ image-gallery/tests/App.test.js
@@ -1,7 +1,16 @@
-// import React from 'react';
-// import {shallow, render} from 'enzyme';
-// import toJson from 'enzyme-to-json';
-// import App from '../src/app';
-// import { List } from '../src/view/list';
-// import { Gallery } from '../src/view/gallery';
-// import { Thumbnail } from '../src/view/thumbnail'
+/* eslint-disable */
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import App from '../src/app';
+import { List } from '../src/view/list';
+import { Gallery } from '../src/view/gallery';
+import { Thumbnail } from '../src/view/thumbnail'
+
+describe('schnoodle lists', () => {
+    it('runs my test!', () => {
+        const rendered = shallow(<App />);
+        expect(toJson(rendered)).toMatchSnapshot();
+    });
+});
