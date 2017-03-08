6bc1d78fc5dcdc4f200bd08bd8546abdd612d02b
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-08 02:27:25 -0800

    cleans up props between ListDisplay and LDBits

--- src/ListDisplayBits.js
+++ src/ListDisplayBits.js
@@ -1,18 +1,16 @@
-// import React, { PropTypes } from 'react';
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 function ListDisplayBits(props) {
-  let image = props.image;
 
-  return <li key={image.id}><a href={image.url}>{image.title}</a>: {image.description}</li>;
+  return <li key={props.id}><a href={props.url}>{props.title}</a>: {props.description}</li>;
 }
 /* TODO: fix PropTypes  */
-// ListDisplayBits.PropTypes = {
-//   key: , // what is new Date()? string?
-//   url: PropTypes.string.isRequired,
-//   title: PropTypes.string.isRequired,
-//   description: PropTypes.string.isRequired,
-// };
+ListDisplayBits.propTypes = {
+  // key: , // what is new Date()? string?
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default ListDisplayBits;
 
