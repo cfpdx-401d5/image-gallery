471fae7253fc47e772746b64bf1773128edf5e33
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-08 10:34:04 -0800

    fixes key warnings in Gallery and Thumbnail Display

--- src/GalleryDisplay.js
+++ src/GalleryDisplay.js
@@ -5,7 +5,7 @@ import GalleryDisplayBits from './GalleryDisplayBits';
 
 function GalleryDisplay(props) {
   const imagesAsArticles = props.images.map(imageObj => {
-    return <GalleryDisplayBits title={imageObj.title} url={imageObj.url} description={imageObj.description}/>;
+    return <GalleryDisplayBits key={imageObj.id} title={imageObj.title} url={imageObj.url} description={imageObj.description}/>;
   });
 
   return (
