2c3748198afad0c0d2b4278863990406237101dd
mericks <mericks@users.noreply.github.com>
2017-03-07 21:58:26 -0800

    add test for ListView -> passing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-import App from '../App';
+import App, { ListView } from '../App';
 
 
 describe('App tests', () => {
@@ -11,5 +11,10 @@ describe('App tests', () => {
         const rendered = shallow(<App />);
         expect(toJson(rendered)).toMatchSnapshot();
     });
+
+    it('ListView', () => {
+        let output = shallow(<ListView />);
+        expect(toJson(output)).toMatchSnapshot();
+    })
 
 });
