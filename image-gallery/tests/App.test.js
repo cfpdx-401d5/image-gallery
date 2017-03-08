52642a71b1e7780613db76914d295f7933cc10b3
Clarefollett <chlaw101@gmail.com>
2017-03-08 11:52:27 -0800

    switching to a new branch for lab two, i will come back and resubmit once i add tests

---
+++ image-gallery/tests/App.test.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import {shallow, render} from 'enzyme';
+// import toJson from 'enzyme-to-json';
+// import App from '../src/app';
+// import { List } from '../src/view/list';
+// import { Gallery } from '../src/view/gallery';
+// import { Thumbnail } from '../src/view/thumbnail'
