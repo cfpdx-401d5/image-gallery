ff0485e26327b21bed1681a0e3d27d6468718705
meghanroserebecca <meghanroserebecca@gmail.com>
2017-03-08 10:06:31 -0800

    switched to branch Dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React, { Component } from 'react';
+import babyBunBuns from '../data/bunnyimages';
+import Thumbnail from './Thumbnail';
+import List from './List';
+import Gallery from './Gallery';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+function Selector(props) {
+  return (
+    <div>
+      <button onClick={() => props.handleClick('thumbnail')}>Thumbnail View</button>
+      <button onClick={() => props.handleClick('list')}>List View</button>
+      <button onClick={() => props.handleClick('gallery')}>Gallery View</button>
+    </div>
+  )
+}
+
+Selector.propTypes = {
+  
+}
+
+function View(props) {
+  if (props.viewStyle === 'thumbnail') {
+    return (
+      <Thumbnail images={props.babyBunBuns}/>
+    )
+  }
+  else if (props.viewStyle === 'list') {
+    return (
+      <List images={props.babyBunBuns}/>
+    )
+  }
+  else if (props.viewStyle === 'gallery') {
+    return (
+      <Gallery images={props.babyBunBuns}/>
+    )
+  }
+}
+
+export default class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewStyle: null
+    };
+    this.handleClick = this.handleclick.bind(this);
+    this.babyBunBuns = babyBunBuns;
+  }
+
+  handleClick(viewValue) {
+    console.log(viewValue);
+    this.setState({
+      viewStyle: viewValue
+    });
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
---
+++ tests/app.test.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { shallow } from 'enzyme'; //this is a renderer that renders to a virtual DOM
+import toJson from 'enzyme-to-json';
