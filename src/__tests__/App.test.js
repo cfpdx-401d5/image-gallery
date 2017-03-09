c0d80ef7d45f8c5e6db2f0768e86f5f995c7c22c
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-08 20:51:08 -0800

    testing progress

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,32 +2,77 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-import App, { ListView, ThumbnailView, GalleryView } from '../App';
+import App, { ViewSelector, ViewDisplay, ListView, ThumbnailView, GalleryView } from '../App';
+import AddResource from '../add-resource';
 
 describe('App', () => {
-    it('runs my test', () => {
+    it('runs my app', () => {
         const renderer = shallow(<App />);
         expect(toJson(renderer)).toMatchSnapshot();
     })
 })
 
-describe('ListView', () => {
-    it('displays list', () => {
-        let output = shallow(<ListView resource= {{title:'a', url: 'b', description: 'c'}}/>);
+describe('ViewSelector', () => {
+    it('selects view', () => {
+        const renderer = shallow(<ViewSelector />);
+        expect(toJson(renderer)).toMatchSnapshot();
+    })
+})
+
+describe('ViewDisplay', () => {
+    it('displays view', () => {
+        const renderer = shallow(<ViewDisplay />);
+        expect(toJson(renderer)).toMatchSnapshot();
+    })
+})
+
+describe('DetailList', () => {
+    it('displays detail list', () => {
+        let output = shallow(<DetailList resource= {{title:'a', url: 'b', description: 'c'}}/>);
         expect(toJson(output)).toMatchSnapshot();
     })
 })
 
-describe('ThumbnailView', () => {
-    it('displays list', () => {
-        let output = shallow(<ThumbnailView resource= {{title:'a', url: 'b', description: 'c'}}/>);
+describe('ThumbnailList', () => {
+    it('displays thumbnail list', () => {
+        let output = shallow(<ThumbnailList resource= {{title:'a', url: 'b', description: 'c'}}/>);
         expect(toJson(output)).toMatchSnapshot();
     })
 })
 
-describe('GalleryView', () => {
-    it('displays list', () => {
-        let output = shallow(<GalleryView resource= {{title:'a', url: 'b', description: 'c'}}/>);
+describe('GalleryList', () => {
+    it('displays gallery list', () => {
+        let output = shallow(<GalleryList resource= {{title:'a', url: 'b', description: 'c'}}/>);
         expect(toJson(output)).toMatchSnapshot();
+    })
+})
+
+describe('DetailDisplay', () => {
+    it('', () => {
+        
+    })
+})
+
+describe('ThumbnailDisplay', () => {
+    it('', () => {
+        
+    })
+})
+
+describe('GalleryDisplay', () => {
+    it('', () => {
+        
+    })
+})
+
+describe('AddResource', () => {
+    it('', () => {
+        
+    })
+})
+
+describe('DeleteResource', () => {
+    it('', () => {
+        
     })
 })
