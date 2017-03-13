ac123f0fab588da7c381af591df6d0eeae9b5096
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-13 11:16:46 -0700

    added fetch functionality for post

--- app/src/components/editing/AddResource.js
+++ app/src/components/editing/AddResource.js
@@ -4,18 +4,46 @@ const TITLE = 'title';
 const DESCRIPTION = 'description';
 const URL = 'url';
 
+function fetcher(options) {
+    const { method, path, body } = options;
+    return fetch(`http://localhost:8000${path}`, {
+        method: method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body),
+    });
+}
+
 export default class AddResource extends Component {
+
+    addRes(newResource) {
+        fetcher({
+            path: '/resources',
+            method: 'POST',
+            body: {
+                title: newResource.title,
+                url: newResource.url,
+                description: newResource.description,
+            }
+        });
+    }
+
     render() {
         return (
             <div>
                 <form onSubmit={(e) => {
+                    e.preventDefault();
                     const newResource = {
-                        id: new Date(),
+                        //id: new Date(),
                         title: this.refs.title.value,
                         url: this.refs.url.value,
                         description: this.refs.description.value,                
                     };
+                    this.addRes(newResource);
                     this.props.onAdd(e, newResource);
+
                 }}>
                     <input ref={TITLE} required placeholder={TITLE}/>
                     <input ref={URL} required placeholder={URL}/>
