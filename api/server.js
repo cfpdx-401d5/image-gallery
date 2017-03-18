54b9e714849e510bdc650440acb81d2c45968086
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-18 13:17:01 -0700

    api changes per github comments

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const app = require('./lib/app');
 require('./lib/connection');
 
 const server = http.createServer(app);
-const port = process.env.dbUri || 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
     console.log('server running', server.address());
